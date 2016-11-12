f27b827983b859b620913c78ef93557f5ad63018
pradeep <pradeep.techhub@gmail.com>
2016-11-13 01:04:17 +0530

    Initial Commit

---
+++ src/main/java/com/patel/pradeep/GitDemoApplication.java
@@ -0,0 +1,12 @@
+package com.patel.pradeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoApplication.class, args);
+	}
+}
